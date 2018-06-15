01ae33e431bc9484454ebac9d23cb1ee9cab5126
danielHeap <dyminskid@gmail.com>
2018-06-15 17:29:49 +0200

    test data

--- Hackathon.Garbaage.Dal/Entities/CordinatesEntity.cs
+++ Hackathon.Garbaage.Dal/Entities/CordinatesEntity.cs
@@ -6,6 +6,13 @@ namespace Hackathon.Garbage.Dal.Entities
 {
     public class CordinatesEntity : IEntity
     {
+        private static int index = 1;
+
+        public CordinatesEntity()
+        {
+            Id = index++;
+        }
+
         public int Id { get; set; }
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
--- Hackathon.Garbaage.Dal/Entities/OrderEntity.cs
+++ Hackathon.Garbaage.Dal/Entities/OrderEntity.cs
@@ -4,8 +4,19 @@ using System.Text;
 
 namespace Hackathon.Garbage.Dal.Entities
 {
+     
     public class OrderEntity : IEntity
     {
+        private static int id = 1;
+        public OrderEntity(FieldEntity field)
+        {
+            Field = field;
+            Id = id++;
+            FieldId = field.Id;
+            DeadlineDate = new DateTime(2018, 06, 11);
+            FinishDate = new DateTime(2018, 06, 10);
+        }
+
         public int Id { get; set; }
         public int FieldId { get; set; }
         public FieldEntity Field { get; set; }
